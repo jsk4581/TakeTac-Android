7583a40a80339ee2e16886108a2a4853e67f65fe
Junggyun827 <jungun0827@gmail.com>
2025-05-22 00:24:49 +0900

    Update HomeActivity.java

--- app/src/main/java/com/team5/taketac/HomeActivity.java
+++ app/src/main/java/com/team5/taketac/HomeActivity.java
@@ -2,6 +2,7 @@ package com.team5.taketac;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +13,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button btnGoToMatching = findViewById(R.id.btnGoToMatching);
-        btnGoToMatching.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, PublicMatchingActivity.class);
-            startActivity(intent);
+        Button btnChatRooms = findViewById(R.id.btnChatRooms);
+        btnChatRooms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ChatRoomListActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
