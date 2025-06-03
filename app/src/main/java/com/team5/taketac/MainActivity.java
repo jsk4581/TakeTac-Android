2775fcdde6e96217828d9f5ce59c2603ab603513
kwon <119kwon@gmail.com>
2025-06-03 17:09:52 +0900

    카카오맵

---
+++ app/src/main/java/com/team5/taketac/MainActivity.java
@@ -0,0 +1,36 @@
+package com.team5.taketac;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(view ->
+                Toast.makeText(MainActivity.this, "다음 화면으로 이동 예정!", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
