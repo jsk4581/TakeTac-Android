454d2ac3fafb4a46c9b86b4d4eecdcfe021713b7
Junggyun827 <jungun0827@gmail.com>
2025-06-04 15:32:54 +0900

    Update Message.java

--- app/src/main/java/com/team5/taketac/model/Message.java
+++ app/src/main/java/com/team5/taketac/model/Message.java
@@ -1,13 +1,18 @@
-// Message.java
 package com.team5.taketac.model;
 
 public class Message {
     private String text;
-    private boolean isSentByUser;
+    private boolean sentByUser;
+    private String senderNickname;  // 닉네임 필드 추가
 
-    public Message(String text, boolean isSentByUser) {
+    public Message() {
+        // Firebase나 다른 곳에서 데이터 바인딩할 때 필요할 수 있음
+    }
+
+    public Message(String text, boolean sentByUser, String senderNickname) {
         this.text = text;
-        this.isSentByUser = isSentByUser;
+        this.sentByUser = sentByUser;
+        this.senderNickname = senderNickname;
     }
 
     public String getText() {
@@ -15,6 +20,24 @@ public class Message {
     }
 
     public boolean isSentByUser() {
-        return isSentByUser;
+        return sentByUser;
     }
+
+    public String getSenderNickname() {
+        return senderNickname;
+    }
+
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setSentByUser(boolean sentByUser) {
+        this.sentByUser = sentByUser;
+    }
+
+    public void setSenderNickname(String senderNickname) {
+        this.senderNickname = senderNickname;
+    }
+
 }
