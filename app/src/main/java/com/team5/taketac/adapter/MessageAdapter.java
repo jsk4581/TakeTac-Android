463bce0c3741e65dd89f400e822ec712a712dc68
Junggyun827 <jungun0827@gmail.com>
2025-06-04 15:32:09 +0900

    Update MessageAdapter.java

--- app/src/main/java/com/team5/taketac/adapter/MessageAdapter.java
+++ app/src/main/java/com/team5/taketac/adapter/MessageAdapter.java
@@ -1,9 +1,10 @@
-// MessageAdapter.java
 package com.team5.taketac.adapter;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,12 +35,18 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
         Message message = messages.get(position);
         holder.textView.setText(message.getText());
 
-        // Alignment based on sender
         if (message.isSentByUser()) {
+            // 내 메시지: 닉네임 숨김, 오른쪽 정렬
+            holder.nicknameText.setVisibility(View.GONE);
             holder.textView.setBackgroundResource(R.drawable.bg_sent);
+            holder.messageContainer.setGravity(Gravity.END);   // 오른쪽 정렬
             holder.textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
         } else {
+            // 상대 메시지: 닉네임 보임, 왼쪽 정렬
+            holder.nicknameText.setVisibility(View.VISIBLE);
+            holder.nicknameText.setText(message.getSenderNickname());
             holder.textView.setBackgroundResource(R.drawable.bg_received);
+            holder.messageContainer.setGravity(Gravity.START); // 왼쪽 정렬
             holder.textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
         }
     }
@@ -50,11 +57,15 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     }
 
     static class MessageViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout messageContainer;
         TextView textView;
+        TextView nicknameText;
 
         public MessageViewHolder(@NonNull View itemView) {
             super(itemView);
+            messageContainer = itemView.findViewById(R.id.messageContainer);
             textView = itemView.findViewById(R.id.messageText);
+            nicknameText = itemView.findViewById(R.id.nicknameText);
         }
     }
 }
